bf6a406e1e48454501f40349b4362364d3e13201
Colm Baston <colm@colmbaston.co.uk>
2018-06-06 18:16:20 +0100

    implemented core behaviour

--- polytile.hs
+++ polytile.hs
@@ -1,3 +1,4 @@
+import           Data.Char
 import           Data.List
 import           Data.Tuple
 import qualified Data.Set as S
@@ -5,45 +6,31 @@ import           Data.Set (Set)
 import qualified Data.Map as M
 import           Data.Map (Map)
 import           Data.Bifunctor
-import           Control.Arrow
+import           System.Environment
+import           System.Exit
 
-import Data.Time.Clock
+type Poly = Set (Int, Int)
+type Grid = Set (Int, Int)
 
-type XY    = (Int, Int)
-type Piece = Set XY
-type Grid  = Set XY
-
-offset :: XY -> Piece -> Piece
+offset :: (Int, Int) -> Poly -> Poly
 offset (x,y) = S.mapMonotonic (bimap (+x) (+y))
 
-normalise :: Piece -> Piece
+normalise :: Poly -> Poly
 normalise p = offset (bimap negate negate (minimum p)) p
 
-i :: Piece
-i = S.fromList [(0,0),(0,1),(0,2),(0,3)]
+polyominoes :: Map String Poly
+polyominoes = M.fromAscList [("I", S.fromList [(0, 0),(0, 1),(0,2),(0,3)]),
+                             ("J", S.fromList [(1,-2),(1,-1),(0,0),(1,0)]),
+                             ("L", S.fromList [(0, 0),(0, 1),(0,2),(1,2)]),
+                             ("O", S.fromList [(0, 0),(1, 0),(0,1),(1,1)]),
+                             ("S", S.fromList [(1,-1),(2,-1),(0,0),(1,0)]),
+                             ("T", S.fromList [(0, 0),(1, 0),(2,0),(1,1)]),
+                             ("Z", S.fromList [(0, 0),(1, 0),(1,1),(2,1)])]
 
-j :: Piece
-j = S.fromList [(1,-2),(1,-1),(0,0),(1,0)]
+grid :: Int -> Int -> Grid
+grid w h = S.fromList [(x,y) | x <- [0..w-1], y <- [0..h-1]]
 
-l :: Piece
-l = S.fromList [(0,0),(0,1),(0,2),(1,2)]
-
-o :: Piece
-o = S.fromList [(0,0),(1,0),(0,1),(1,1)]
-
-s :: Piece
-s = S.fromList [(1,-1),(2,-1),(0,0),(1,0)]
-
-t :: Piece
-t = S.fromList [(0,0),(1,0),(2,0),(1,1)]
-
-z :: Piece
-z = S.fromList [(0,0),(1,0),(1,1),(2,1)]
-
-grid :: XY -> Grid
-grid (x,y) = S.fromList [(x',y') | x' <- [0..x-1], y' <- [0..y-1]]
-
-rotations :: Piece -> [Piece]
+rotations :: Poly -> [Poly]
 rotations = map head . group . sort . map normalise . take 4 . iterate (S.map (swap . fmap negate))
 
 uniquePerms :: Ord a => [a] -> [[a]]
@@ -57,3 +44,44 @@ uniquePerms = go . group . sort
     selects :: [a] -> [(a,[a])]
     selects []     = []
     selects (x:xs) = (x,xs) : map (fmap (x:)) (selects xs)
+
+tile :: Int -> Int -> [Poly] -> [[Poly]]
+tile x y ps = uniquePerms ps >>= go (grid x y)
+  where
+    go :: Grid -> [Poly] -> [[Poly]]
+    go _ []     = [[]]
+    go g (p:ps) | sg < sp   = []
+                | otherwise = do r <- offset (minimum g) <$> rotations p
+                                 let g' = g S.\\ r
+                                 if S.size g' + sp /= sg
+                                   then []
+                                   else (r:) <$> go g' ps
+                where
+                  sg = S.size g
+                  sp = S.size p
+
+usageFail :: IO a
+usageFail = do p <- getProgName
+               putStrLn ("Usage: " ++ p ++ " width height [space-separated, non-empty list of polyominoes]")
+               putStrLn (" - Available polyominoes: " ++ unwords (M.keys polyominoes))
+               exitFailure
+
+parseArgs :: [String] -> IO (Int, Int, [Poly])
+parseArgs (w:h:as) | all isDigit (w ++ h) && not (null as) = case mapM (`M.lookup` polyominoes) as of
+                                                               Nothing -> usageFail
+                                                               Just ps -> pure (read w, read h, ps)
+parseArgs _        = usageFail
+
+main :: IO ()
+main = do (w, h, ps) <- getArgs >>= parseArgs
+          if w * h < sum (map S.size ps)
+            then putStrLn "Cannot tile: too many polyominoes!"
+            else do putStrLn "Searching for tiling..."
+                    case tile w h ps of
+                      []    ->    putStrLn "No tilings found!"
+                      (t:_) -> do putStrLn ""
+                                  sequence_ (intersperse (putStrLn "") (map (drawPoly w h) t))
+                                  putStrLn ""
+
+drawPoly :: Int -> Int -> Poly -> IO ()
+drawPoly w h p = mapM_ (putStrLn . (' ':)) [intersperse ' ' [if (x,y) `elem` p then 'o' else '-' | x <- [0..w-1]] | y <- [0..h-1]]
